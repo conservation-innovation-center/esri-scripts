6da5de75d949622655e387e035f9fdaad3a6a416
Louis15 <31776428+Louis15@users.noreply.github.com>
2017-09-20 14:01:20 -0400

    ExtractbyMask FOR loop

--- ExtractbyMask_Louis.py
+++ ExtractbyMask_Louis.py
@@ -10,7 +10,9 @@ arcpy.env.overwriteOutput = True
 water_ws = "H:/VEE_data/Analysis/HUC10_Water_network"
 mask_ws = "H:/VEE_data/Analysis/HUC10_filled"
 
-#for loop to loop through all file pairs
+#for loop to loop through all file pairs. %(i) generates a consecutive integer each time the loop runs, which is inserted in place of %s, which substitutes 
+#a string value in the file name. http://infohost.nmt.edu/tcc/help/pubs/python25/web/str-format.html
+#By giving each file the same number, they are paired and run by the ExtractbyMask tool. This tool only works if files in each folder are in the same order.
 for i in range (0, 46):
     #set input rasters
     water_raster = "H:/VEE_data/Analysis/HUC10_Water_network/Water_network_%s_fill.tif" %(i)
